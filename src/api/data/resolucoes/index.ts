5e2bd6381d6970872ffdbdea6a691695d85381e5
Tamiris <btamiris73@gmail.com>
2021-02-23 13:09:13 -0300

    Projeto final

--- src/api/data/resolucoes/index.ts
+++ src/api/data/resolucoes/index.ts
@@ -2,8 +2,8 @@ import api from "../../index";
 import { IResolucao } from "../../../interfaces/resolucao.interface"
 
 class resolucaoData {
-  show(questoes_id: string, gabarito: string) {
-    return api.get<IResolucao[]>(`resolucao/${questoes_id}, resolucao/${gabarito}`);
+  show(gabarito: string) {
+    return api.get<IResolucao[]>(`resolucao/${gabarito}`);
   }
 }
 
